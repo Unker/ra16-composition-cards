3c4db30849d1ea276c25dca4213838f1644c65e9
unker <kfi.unknown@gmail.com>
2024-01-04 22:52:15 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Card from './Card'
+
+function App() {
+
+  return (
+    <>
+      <Card 
+        title='Card title' 
+        text="Some quick example text to build on the card title and make up the bulk of the card's content." 
+        link='.' 
+        refName='Click!'
+      />
+    </>
+  )
+}
+
+export default App
---
+++ src/Card.tsx
@@ -0,0 +1,30 @@
+import './Card.css'
+import 'bootstrap/dist/css/bootstrap.min.css';  
+
+type CardProps = {
+  img?: string;
+  title: string;
+  text: string;
+  link: string;
+  refName: string;
+}
+
+const Card = ({ img, title, text, link, refName }: CardProps) => {
+
+  return (
+    <div className="card">
+      {img ? (
+        <img src={img} className="card-img-top" alt="..."></img>
+      ) : (
+        <></>
+      )}
+      <div className="card-body">
+        <h5 className="card-title">{title}</h5>
+        <p className="card-text">{text}</p>
+        <a href={link} className="btn btn-primary">{refName}</a>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
